7b4cd968f8ac5e0349154d09b5fd194c13d38d5f
FernandoJRR <fercitotron@gmail.com>
2025-05-30 14:02:14 -0600

    feat: add basic sidebar and inscription form

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,15 +1,95 @@
 <template>
-  <div >
+  <div>
     <!-- <header>
       <h1>My Application</h1>
     </header> -->
-    <main class="px-4 py-6">
-      <NuxtPage />
-    </main>
+    <SidebarProvider>
+      <AppSidebar />
+      <main>
+        <div class="flex flex-row mb-4 p-2">
+          <SidebarTrigger />
+          <h1>Compdes</h1>
+        </div>
+        <NuxtPage />
+      </main>
+    </SidebarProvider>
+    <Drawer>
+      <DrawerTrigger as-child
+        class="fixed bottom-0 hover:-translate-y-2 transition right-0 py-3 border-t border-neutral-200 w-full lg:w-1/2">
+        <button class="">Preguntas frecuentes</button>
+      </DrawerTrigger>
+      <DrawerContent>
+        <div class="mx-auto w-full max-w-lg overflow-auto pb-6">
+          <DrawerHeader class="sticky top-0 bg-white">
+            <DrawerTitle class="flex justify-between">
+              Preguntas frecuentes
+              <DrawerClose as-child>
+                <Button variant="ghost" size="icon">
+                  <Icon name="lucide:x" />
+                </Button>
+              </DrawerClose>
+            </DrawerTitle>
+          </DrawerHeader>
+          <div class="p-4 pb-0">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla
+            officia iure neque fugiat aliquam eos ab provident ducimus.
+            Assumenda dolore quas tempora adipisci a provident sapiente debitis
+            aspernatur aliquid animi.
+          </div>
+          <div class="p-4 pb-0">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla
+            officia iure neque fugiat aliquam eos ab provident ducimus.
+            Assumenda dolore quas tempora adipisci a provident sapiente debitis
+            aspernatur aliquid animi.
+          </div>
+          <div class="p-4 pb-0">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla
+            officia iure neque fugiat aliquam eos ab provident ducimus.
+            Assumenda dolore quas tempora adipisci a provident sapiente debitis
+            aspernatur aliquid animi.
+          </div>
+          <div class="p-4 pb-0">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla
+            officia iure neque fugiat aliquam eos ab provident ducimus.
+            Assumenda dolore quas tempora adipisci a provident sapiente debitis
+            aspernatur aliquid animi.
+          </div>
+          <div class="p-4 pb-0">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla
+            officia iure neque fugiat aliquam eos ab provident ducimus.
+            Assumenda dolore quas tempora adipisci a provident sapiente debitis
+            aspernatur aliquid animi.
+          </div>
+          <div class="p-4 pb-0">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla
+            officia iure neque fugiat aliquam eos ab provident ducimus.
+            Assumenda dolore quas tempora adipisci a provident sapiente debitis
+            aspernatur aliquid animi.
+          </div>
+          <div class="p-4 pb-0">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla
+            officia iure neque fugiat aliquam eos ab provident ducimus.
+            Assumenda dolore quas tempora adipisci a provident sapiente debitis
+            aspernatur aliquid animi.
+          </div>
+          <div class="p-4 pb-0">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla
+            officia iure neque fugiat aliquam eos ab provident ducimus.
+            Assumenda dolore quas tempora adipisci a provident sapiente debitis
+            aspernatur aliquid animi.
+          </div>
+        </div>
+      </DrawerContent>
+    </Drawer>
     <!-- <footer>
       <p>&copy; 2023 My Application</p>
     </footer> -->
   </div>
 </template>
-<script setup></script>
+<script setup>
+import AppSidebar from '~/components/AppSidebar.vue';
+import SidebarProvider from '~/components/ui/sidebar/SidebarProvider.vue';
+import SidebarTrigger from '~/components/ui/sidebar/SidebarTrigger.vue';
+
+</script>
 <style scoped></style>
--- app/pages/inscripcion/index.vue
+++ app/pages/inscripcion/index.vue
@@ -1,101 +1,157 @@
-<script setup lang="ts">
-  import { Button } from "@/components/ui/button";
-  import {
-    Stepper,
-    StepperDescription,
-    StepperItem,
-    StepperSeparator,
-    StepperTitle,
-    StepperTrigger,
-  } from "@/components/ui/stepper";
-
-  const stepIndex = ref(1);
-  const steps = [
-    {
-      step: 1,
-      title: "Your details",
-      description: "Provide your name and email",
-      icon: "lucide:user",
-    },
-    {
-      step: 2,
-      title: "Company details",
-      description: "A few details about your company",
-      icon: "lucide:wallet",
-    },
-    {
-      step: 3,
-      title: "Invite your team",
-      description: "Start collaborating with your team",
-      icon: "lucide:receipt",
-    },
-    {
-      step: 4,
-      title: "Invite your team",
-      description: "Start collaborating with your team",
-      icon: "lucide:users",
-    },
-  ];
-</script>
-
 <template>
-  <Button
-    variant="outline"
-    class="mb-4"
-    @click="() => stepIndex--"
-    :disabled="stepIndex === 1"
-  >
-    Previous Step
-  </Button>
-  <Button
-    variant="outline"
-    class="mb-4"
-    @click="() => stepIndex++"
-    :disabled="stepIndex === steps.length"
-  >
-    Next Step
-  </Button>
-  <Stepper class="flex w-full items-start gap-2" v-model="stepIndex">
-    <StepperItem
-      v-for="step in steps"
-      :key="step.step"
-      v-slot="{ state }"
-      class="relative flex w-full flex-col items-center justify-center"
-      :step="step.step"
-    >
-      <StepperSeparator
-        v-if="step.step !== steps[steps.length - 1].step"
-        class="absolute left-[calc(50%+40px)] right-[calc(-50%+30px)] top-5 block h-0.5 shrink-0 rounded-full bg-muted group-data-[state=completed]:bg-primary"
-      />
+  <Form >
+    <Stepper class="flex w-full items-start gap-2 relative" v-model="stepIndex">
+      <StepperItem v-for="step in steps" :key="step.step" v-slot="{ state }"
+        class="flex w-full flex-col items-center justify-center" :step="step.step">
+        <StepperSeparator v-if="step.step !== steps[steps.length - 1].step"
+          class="absolute left-[calc(0%+40px)] right-[calc(-0%+30px)] top-5 block h-0.5 shrink-0 rounded-full bg-muted group-data-[state=completed]:bg-primary" />
 
-      <div
-        class="z-10 rounded-full shrink-0 p-2"
-        :class="[
+        <div class="z-10 rounded-full shrink-0 p-2" :class="[
           state === 'active' &&
-            'outline-2 outline-ring outline-offset-2',
-        ]"
-        @click="() => undefined"
-      >
-        <Icon name="lucide:check" v-if="state === 'completed'" />
-        <!-- <Icon name="lucide:circle" v-if="state === 'active'" /> -->
-        <Icon :name="step.icon" v-else />
-        <!-- <Icon name="lucide:dot" v-if="state === 'inactive'" /> -->
-      </div>
+          'outline-2 outline-ring outline-offset-2',
+        ]" @click="() => undefined">
+          <Icon name="lucide:check" v-if="state === 'completed'" />
+          <Icon :name="step.icon" v-else />
+        </div>
 
-      <!-- <div class="mt-5 flex flex-col items-center text-center">
-        <StepperTitle
-          :class="[state === 'active' && 'text-primary']"
-          class="text-sm font-semibold transition lg:text-base"
-        >
+        <StepperTitle :class="[state === 'active' && 'text-primary']"
+          class="absolute top-full mt-4 w-max -translate-x-1/2 left-1/2 text-xl font-semibold transition lg:text-base"
+          v-if="state === 'active'">
           {{ step.title }}
         </StepperTitle>
-        <StepperDescription
-          :class="[state === 'active' && 'text-primary']"
-          class="sr-only text-xs text-muted-foreground transition md:not-sr-only lg:text-sm"
-        >
+        <!-- 
+      <div class="mt-5 flex flex-col items-center text-center">
+        <StepperDescription :class="[state === 'active' && 'text-primary']"
+          class="sr-only text-xs text-muted-foreground transition md:not-sr-only lg:text-sm">
           {{ step.description }}
         </StepperDescription>
-      </div> -->
-    </StepperItem>
-  </Stepper>
+      </div>
+-->
+      </StepperItem>
+    </Stepper>
+    <div class="flex flex-col gap-4 mt-16 items-center">
+      <template v-if="stepIndex === 1">
+        <div class="flex flex-row px-2 gap-x-2">
+          <FormField name="firstName">
+            <FormItem>
+              <FormLabel>Nombre</FormLabel>
+              <FormControl>
+                <Input type="text" />
+              </FormControl>
+            </FormItem>
+          </FormField>
+          <FormField name="lastName">
+            <FormItem>
+              <FormLabel>Apellido</FormLabel>
+              <FormControl>
+                <Input type="text" />
+              </FormControl>
+            </FormItem>
+          </FormField>
+        </div>
+        <FormField name="id">
+          <FormItem>
+            <FormLabel>ID</FormLabel>
+            <FormControl>
+              <Input type="text" />
+            </FormControl>
+          </FormItem>
+        </FormField>
+        <FormField name="phone">
+          <FormItem>
+            <FormLabel>Telefono</FormLabel>
+            <FormControl>
+              <Input type="text" />
+            </FormControl>
+          </FormItem>
+        </FormField>
+        <FormField name="email">
+          <FormItem>
+            <FormLabel>Correo Electronico</FormLabel>
+            <FormControl>
+              <Input type="text" />
+            </FormControl>
+          </FormItem>
+        </FormField>
+        <FormField name="university">
+          <FormItem>
+            <FormLabel>Universidad</FormLabel>
+            <FormControl>
+              <Input type="text" />
+            </FormControl>
+          </FormItem>
+        </FormField>
+      </template>
+
+      <template v-if="stepIndex === 2">
+        <h1>Lorem ipsum instruccion</h1>
+      </template>
+
+      <template v-if="stepIndex === 3">
+        <h1>Link de Pago</h1>
+      </template>
+
+      <template v-if="stepIndex === 4">
+        <h1>Nombre</h1>
+        <h1>Apellido</h1>
+        <h1>ID</h1>
+        <h1>Telefono</h1>
+        <h1>Correo Electronico</h1>
+        <h1>Universidad</h1>
+      </template>
+    </div>
+    <div class="absolute bottom-12 left-0 w-full grid grid-cols-2">
+      <Button variant="outline" class="rounded-none" @click="() => stepIndex--" :disabled="stepIndex === 1" type="button">
+        Anterior
+      </Button>
+      <Button variant="outline" class="rounded-none" @click="() => stepIndex++" :disabled="stepIndex === steps.length" type="button">
+        Siguiente
+      </Button>
+    </div>
+  </Form>
 </template>
+<script setup lang="ts">
+import { FormField } from "#components";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Form } from "@/components/ui/form";
+import {
+  Stepper,
+  StepperDescription,
+  StepperItem,
+  StepperSeparator,
+  StepperTitle,
+  StepperTrigger,
+} from "@/components/ui/stepper";
+import FormControl from "~/components/ui/form/FormControl.vue";
+import FormItem from "~/components/ui/form/FormItem.vue";
+import FormLabel from "~/components/ui/form/FormLabel.vue";
+
+const stepIndex = ref(1);
+const steps = [
+  {
+    step: 1,
+    title: "Datos Personales",
+    description: "Provide your name and email",
+    icon: "lucide:user",
+  },
+  {
+    step: 2,
+    title: "Instrucciones de Pago",
+    description: "A few details about your company",
+    icon: "lucide:wallet",
+  },
+  {
+    step: 3,
+    title: "Comprobacion de Pago",
+    description: "Start collaborating with your team",
+    icon: "lucide:receipt",
+  },
+  {
+    step: 4,
+    title: "Confirmacion de Datos",
+    description: "Start collaborating with your team",
+    icon: "lucide:users",
+  },
+];
+</script>
