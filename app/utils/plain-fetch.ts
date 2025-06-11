7b9f055a7908e6263d7ff62ad3e7ccf36c88538f
Roberto Bautista <44622828+rrobertobt@users.noreply.github.com>
2025-06-10 22:42:32 -0600

    fix: Update baseURL to a hardcoded API endpoint in plain-fetch utility

--- app/utils/plain-fetch.ts
+++ app/utils/plain-fetch.ts
@@ -1,7 +1,8 @@
 import { COOKIE_TOKEN_KEY } from "~/plugins/01.authCookie"
 
 export const $api = $fetch.create({
-  baseURL: `${import.meta.env.VITE_API_HOST}`,
+  // baseURL: `${import.meta.env.VITE_API_HOST}`,
+  baseURL: "https://compdes.cunoc.edu.gt/api/api/v1",
   onRequest({ options }) {
     const token = useCookie(COOKIE_TOKEN_KEY).value
     options.headers.set('Authorization', `Bearer ${token}`)
