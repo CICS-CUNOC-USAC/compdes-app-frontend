00b245365f83dd2c396da0541c5756d7c0154656
Roberto Bautista <44622828+rrobertobt@users.noreply.github.com>
2025-06-11 19:29:55 -0600

    fix: restore baseURL to use environment variable for API endpoint

--- app/utils/plain-fetch.ts
+++ app/utils/plain-fetch.ts
@@ -1,8 +1,7 @@
 import { COOKIE_TOKEN_KEY } from "~/plugins/01.authCookie"
 
 export const $api = $fetch.create({
-  // baseURL: `${import.meta.env.VITE_API_HOST}`,
-  baseURL: "https://compdes.cunoc.edu.gt/api/api/v1",
+  baseURL: `${import.meta.env.VITE_API_HOST}`,
   onRequest({ options }) {
     const token = useCookie(COOKIE_TOKEN_KEY).value
     options.headers.set('Authorization', `Bearer ${token}`)
